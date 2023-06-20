f2b163c9ef50a2dfcc0e8b46455964d4cf6463f9
bkhadher <bkhadher@it-koncept.com>
2023-06-20 02:04:26 +0200

    change model auth from User to Customer

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -11,6 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Fortify\Fortify;
+use App\Models\Customer;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -31,6 +32,16 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
         Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
         Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+
+        Fortify::authenticateUsing(function (Request $request) {
+            $user = Customer::where('email', $request->email)
+                            ->where('password', $request->password)
+                                    ->first();
+     
+            if ($user) {
+                return $user;
+            }
+        });
 
         RateLimiter::for('login', function (Request $request) {
             $email = (string) $request->email;
